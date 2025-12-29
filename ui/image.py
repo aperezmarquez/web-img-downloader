3f3b5d1ace75454641b5cce74f74da03a9ebba8c
aperezmarquez <aperezmarquez.business@gmail.com>
2025-12-29 16:46:50 +0100

    Added: Basic update for the image, missing reactivex

--- ui/image.py
+++ ui/image.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+def update_image(image_canvas):
+    # Code to get notify of a change (new image selected)
+    image_canvas.delete("all")
+
+    new_image = Image.open("../assets/placeholder.png") # Change this with the url of the new selected img
+    new_photo = ImageTk.PhotoImage(new_image)
+
+    image_canvas.create_image(0, 0, anchor=tk.NW, image=new_photo)
+    image_canvas.image = new_photo
+
 def image_loader(root, width, height):
     loader_frame = tk.Frame(root)
     loader_frame.pack(side=tk.RIGHT, padx=10, pady=10)
