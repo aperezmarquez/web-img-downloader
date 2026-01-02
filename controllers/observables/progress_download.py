ecadcfff621f54102f7d5249924bc6825045e772
aperezmarquez <aperezmarquez.business@gmail.com>
2026-01-03 00:02:04 +0100

    Update: Comments for the progress bar subject an TkInter element

--- controllers/observables/progress_download.py
+++ controllers/observables/progress_download.py
@@ -1,5 +1,13 @@
 from rx.subject import Subject
 
+# SUBJECT FOR THE SELECTED IMAGE
+# - Params:
+#   - max: the max value of the progress bar
+# - Functions:
+#   - subscribe: returns the subject subscription
+#   - update_progress: updates the progress value and notifies the observers
+#   - get_max: returns the max value of the progress bar
+# - Description: Subject for the progress of the download to show visually to the user
 class ProgressSubject:
     def __init__(self, max):
         self.subject = Subject()
--- ui/progress_bar.py
+++ ui/progress_bar.py
@@ -3,23 +3,43 @@ from tkinter import ttk
 progressbar = None
 btn = None
 
+# SUBSCRIBE TO THE PROGRESS SUBJECT
+# - Params:
+#   - progress_sub: the progress subject to subscribe
+# - Return: None
+# - Description: Subscribes to the progress subject and updates the progress bar
 def update_progress_sub(progress_sub):
     global progressbar
     progressbar["maximum"] = progress_sub.get_max()
     progress_sub.subscribe(lambda v: change_progress(v))
     btn["state"] = "disabled"
 
+# ENABLES THE DOWNLOAD BUTTON
+# - Params: None
+# - Return: None
+# - Description: Enables the download button
 def enable_btn():
     global btn
     btn["state"] = "normal"
 
+# UPDATES THE PROGRESS BAR
+# - Params:
+#   - value: the new value for the progress bar
+# - Return: None
+# - Description: Updates the progress bar
 def change_progress(value):
     global progressbar
     progressbar["value"] = progressbar["maximum"] - value
 
+    # If the download is done the download button is ready to use again
     if progressbar["value"] == progressbar["maximum"]:
         enable_btn()
 
+# PROGRESS BAR TO SHOW THE DOWNLOAD PROGRESS
+# - Params:
+#   - root: the window root
+#   - button: the button that starts the download
+# - Description: Creates the progress bar
 def progress_bar(root, button):
     global progressbar, btn
     btn = button
